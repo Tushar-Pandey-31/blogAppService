9f9ec1ca3a53dc65081dd78affce359c4151d099
tuxsharx <pandeytushar359@gmail.com>
2025-06-04 16:37:02 +0530

    project setup

---
+++ src/main/java/com/assignment/blogappservice/BlogAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.blogappservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogAppServiceApplication.class, args);
+    }
+
+}
